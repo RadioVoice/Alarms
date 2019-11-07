e225ed098fe9587abd45057797b44b9941c8daf6
Daniel Kessler <djk172@case.edu>
2019-11-07 10:31:55 -0500

    Added CameraView class

--- src/CameraView.java
+++ src/CameraView.java
@@ -1,3 +1,44 @@
+import javax.naming.directory.InvalidAttributeValueException;
+import java.util.Objects;
+
 public class CameraView {
-    //can be null
+    private final int width;
+    private final int height;
+    private final int[][] data;
+
+    private CameraView(int x, int y) {
+        width = x;
+        height = y;
+        data = new int[x][y];
+    }
+
+    public static CameraView of(int x, int y){
+        assert(x>0 && y>0);
+        return new CameraView(x, y);
+    }
+
+    public static CameraView of(int x, int y, String inputData){
+        assert(x>0 && y>0);
+        Objects.requireNonNull(inputData);
+
+        CameraView returnView = new CameraView(x, y);
+        byte[] inputBytes = inputData.getBytes();
+        int byteIndex = 0;
+
+        for (int j = 0; j < y; j++){
+            for (int i = 0; i < x; i++){
+                returnView.data[i][j] = inputBytes[byteIndex];
+            }
+        }
+
+        return returnView;
+    }
+
+    void setValue(int x, int y, int value){
+        data[x][y] = value;
+    }
+
+    int getValue(int x, int y){
+        return data[x][y];
+    }
 }
--- src/State.java
+++ src/State.java
@@ -1,5 +1,7 @@
 public class State {
     //contains 3 camera views
+    private CameraView frontView, sideView, topView;
+
 
     //isDifferent(State)
     //  Returns true if this state is different from the input state
