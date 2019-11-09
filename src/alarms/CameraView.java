459594e379274495e200f6b5c378b3946e833ec9
YianShi <yxs600@case.com>
2019-11-09 11:51:45 -0500

    use testhook to test private methods

--- src/alarms/CameraView.java
+++ src/alarms/CameraView.java
@@ -43,7 +43,8 @@ public class CameraView {
 	}
 
 	// get all possible shifted views
-	static List<CameraView> possibleShiftedViews(CameraView view) {
+	public static List<CameraView> possibleShiftedViews(CameraView view) {
+		Objects.requireNonNull(view);
 		List<CameraView> possibleShifts = new ArrayList<>();
 		int[][] container;
 		for (int i = 0; i <= view.xDim * 2 - 2; i++) {
@@ -59,6 +60,8 @@ public class CameraView {
 
 	// return emptyContainer
 	private static int[][] emptyContainer(int rowNum, int colNum) {
+		Objects.requireNonNull(rowNum);
+		Objects.requireNonNull(colNum);
 		int[][] container = new int[rowNum][colNum];
 		for (int i = 0; i < container.length; i++) {
 			Arrays.fill(container[i], 0);
@@ -68,6 +71,10 @@ public class CameraView {
 
 	// copy 2d array
 	private static void copyViewToLocation(int[][] source, int[][] dest, int startI, int startJ) {
+		Objects.requireNonNull(source);
+		Objects.requireNonNull(dest);
+		Objects.requireNonNull(startI);
+		Objects.requireNonNull(startJ);
 		for (int i = 0; i < source.length; i++) {
 			for (int j = 0; j < source[0].length; j++) {
 				dest[startI + i][startJ + j] = source[i][j];
@@ -77,6 +84,11 @@ public class CameraView {
 
 	// return a part of a 2d array
 	private static int[][] trimmedView(int[][] source, int startI, int startJ, int endI, int endJ) {
+		Objects.requireNonNull(source);
+		Objects.requireNonNull(startI);
+		Objects.requireNonNull(startJ);
+		Objects.requireNonNull(endI);
+		Objects.requireNonNull(endJ);
 		int[][] part = new int[endI - startI][endJ - startJ];
 		for (int i = startI; i < endI; i++) {
 			part[i - startI] = Arrays.copyOfRange(source[i], startJ, endJ);
@@ -88,6 +100,19 @@ public class CameraView {
 	public boolean equals(Object other) {
 		return other instanceof CameraView && ((CameraView) other).cameraDirection.equals(cameraDirection)
 				&& ((CameraView) other).data.equals(data);
+	}
+	
+	// test private methods
+	public static class TESTHOOK {
+		public static int[][] testEmptyContainer(int rowNum, int colNum) {
+			return emptyContainer(rowNum, colNum);
+		}
+		public static void testCopyViewToLocation(int[][] source, int[][] dest, int startI, int startJ) {
+			copyViewToLocation(source, dest, startI, startJ);
+		}
+		public static int[][] testTrimmedView(int[][] source, int startI, int startJ, int endI, int endJ){
+			return trimmedView(source, startI, startJ, endI, endJ);
+		}
 	}
 
 }
